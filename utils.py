a7ae8806c7b1515b003964762cd7d2853408b726
kkwann <40452716+kkwann@users.noreply.github.com>
2024-08-07 15:37:56 +0900

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import streamlit as st
 import pandas as pd
-import geopandas as gpd
-import pandas_gbq
+# import geopandas as gpd
+# import pandas_gbq
 from datetime import datetime, timedelta
 import pytz
 import json
